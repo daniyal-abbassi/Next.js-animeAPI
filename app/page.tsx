5959525e2cb393ae40e585338b4120fa5249a85e
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-03 02:34:11 +0330

    chore: Add temporary layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,21 @@ export default function Page() {
     <>
     <Navbar />
     <div className="container-fluid min-vh-100 d-flex flex-column justify-content-start align-items-center p-4 h4">
-      <div className="anime-list border rounded-3 p-4 container-fluid d-flex flex-row justify-content-between flex-wrap">
-      
+      <div className="search-box">
+        <h2 className="rounded-pill p-3 bg-primary">THIS IS(gonna be) SEARCH BOX HERE!!!!!</h2>
+      </div>
+      <div className="row anime-list  p-4 container-fluid">
+      <div className="col-4  border border-primary p-3 mx-4">
+      <div className="filter-box">
+        <p className="text-bg-warning h4">FILTERS GOES HERE</p>
+      </div>
+      </div>
+      <div className="col-7 border rounded-3 d-flex flex-row justify-content-between flex-wrap">
+        
       <Board />
+
+        
+      </div>
       </div>
       
    
--- app/ui/board.tsx
+++ app/ui/board.tsx
@@ -9,7 +9,7 @@ export default async function Board() {
       {results?.data &&
         results.data.map((anime: any) => (
           <div className="borad-card border rounded-3 border-white  m-3 " key={anime.mal_id}>
-            <Image className="w-100 mb-3" src={`${anime.images.jpg.large_image_url}`} alt={anime.title} width={300} height={420} />
+            <Image className="w-100 mb-3" src={`${anime.images.jpg.large_image_url}`} alt={anime.title} width={260} height={400} />
             <p>{anime.title_english || anime.title}</p>
           </div>
         ))}
