cdb6d2b6f91ace2a266356ede827747dc6e13a96
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-03 00:52:30 +0330

    chore: add list

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ export default function Page() {
   return (
     <>
     <Navbar />
-    <div className="container-fluid min-vh-100 d-flex flex-column justify-content-center align-items-center p-4">
+    <div className="container-fluid min-vh-100 d-flex flex-column justify-content-start align-items-center p-4 h4">
       
-      <h1>Hello World</h1>
+      <ul className="list-group p-4">
+        <li className="list-group-item">anime -1</li>
+        <li className="list-group-item">anime -2</li>
+        <li className="list-group-item">anime -3</li>
+      </ul>
       
    
     </div>
