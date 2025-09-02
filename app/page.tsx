63426ee6585dcfdb0974fc4dd47e5a3d5b586f6c
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-03 01:25:49 +0330

    chore: Fetch from jikan api

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import List from "./ui/list";
 import Navbar from "./ui/navbar";
 
 export default function Page() {
@@ -6,11 +7,7 @@ export default function Page() {
     <Navbar />
     <div className="container-fluid min-vh-100 d-flex flex-column justify-content-start align-items-center p-4 h4">
       
-      <ul className="list-group p-4">
-        <li className="list-group-item">anime -1</li>
-        <li className="list-group-item">anime -2</li>
-        <li className="list-group-item">anime -3</li>
-      </ul>
+      <List />
       
    
     </div>
