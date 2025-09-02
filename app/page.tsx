6baf117d2d27dc3b94f56c700fb13075ee51506f
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-03 00:36:38 +0330

    chore: Add nabar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
+import Navbar from "./ui/navbar";
 
-export default function Home() {
+export default function Page() {
   return (
+    <>
+    <Navbar />
     <div className="container-fluid min-vh-100 d-flex flex-column justify-content-center align-items-center p-4">
       
       <h1>Hello World</h1>
       
    
     </div>
+    </>
   );
 }
---
+++ app/ui/navbar.tsx
@@ -0,0 +1,6 @@
+
+
+export default function Navbar() {
+    return(
+    <header className="bg-light text-dark p-3 bold header-title h2 mt-3 mx-3 border border-primary rounded-pill">Next.js Learning Project : <span className="text-primary p-2">Anime API</span></header>
+)}
