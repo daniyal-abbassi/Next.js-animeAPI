145116b710d35b178f30a18fa1816b7f8dd325f1
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-06 01:14:11 +0330

    chore: Implement logic to  keep url and input in synced

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,35 @@
+"use client";
 
+import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
-export default function Search({placeholder}: {placeholder: string}) {
-    return (
-        <div className="input-group input-group-lg">
-            <span className="input-group-text" id="search-by-name">Search Anime</span>
-            <input type="text" className="form-control" aria-label="Search anime by name" aria-describedby="search-by-name"/>
-        </div>
-    );
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+  return (
+    <div className="input-group input-group-lg">
+      <span className="input-group-text" id="search-by-name">
+        Search Anime
+      </span>
+      <input
+        type="text"
+        className="form-control"
+        aria-label="Search anime by name"
+        aria-describedby="search-by-name"
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
+    </div>
+  );
 }
