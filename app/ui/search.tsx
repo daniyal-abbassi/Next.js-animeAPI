f69614cf580d762f32d9cbb3fde2975c460e1b7a
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-07 04:20:07 +0330

    feat: Add debouncing function

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,21 +2,23 @@
 
 import { useSearchParams } from "next/navigation";
 import { usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+    console.log(term)
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  },500)
   return (
     <div className="input-group input-group-lg">
       <span className="input-group-text text-bg-primary" id="search-by-name">
