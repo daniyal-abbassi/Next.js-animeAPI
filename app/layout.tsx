c243b53fca6769b9cc59e351b870f113258b804f
daniyal-abbassi <slimshady3753@protonmail.com>
2025-10-20 16:01:13 +0330

    feat: Add vercel speedInsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} ${styles.body}`}>
         {children}
+        <SpeedInsights />
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
